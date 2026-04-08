45488669b4e2c88cdf609797d8c07c5b77d61408
aarthicrackers-hub <aarthicrackers@gmail.com>
2026-04-08 23:44:17 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
+import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-import type { NextConfig } from 'next';
 
-// Path to your request configuration
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true, 
-  }
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default withNextIntl(nextConfig);
